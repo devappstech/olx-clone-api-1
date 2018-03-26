73072c24f481a1c93841b4570f8afeddbad1200a
Akash P <akash27797@gmail.com>
2018-03-26 16:20:49 +0530

    added ci and modified some configrations

--- postgres_setup.sh
+++ postgres_setup.sh
@@ -1,5 +1,1 @@
-sudo apt-get install -y postgresql postgresql-client libpq-dev
-sudo -u postgres psql -d template1
-CREATE USER 'postgres' WITH PASSWORD 'postgres' CREATEDB;
-CREATE DATABASE 'postgres' OWNER runner;
-\q
+apt-get install -y postgresql postgresql-client libpq-dev
