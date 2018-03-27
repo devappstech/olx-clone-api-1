c0f7289b446fa59dfb27caff6594fd30dd6a35ed
Akash P <akash27797@gmail.com>
2018-03-27 12:59:53 +0530

    added envs to setup.sh of react app

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export NODE_ENV= production
+export NODE_ENV=production
 export PGHOST=postgres
 export PGUSER=postgres
 export PGPASSWORD=123456
@@ -9,6 +9,8 @@ export PGPORT=5432
 export sessionSecert=someVerySecertK3yThatNoBodyKnow$Actually
 export BASE_URL=http://localhost
 export PORT=3000
+export REACT_PORT=3001
 export emailUser=akash@improwised.com
 export emailPassword=samplepassword
+export API_URL=http://app:3000
 docker-compose up
